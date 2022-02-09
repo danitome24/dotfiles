ed4cf7598f8297399383edb6d4027be5cbae059c
Daniel Tomé Fernández <danieltomefer@gmail.com>
2022-02-09 08:42:21 +0100

    Changes

--- terminal/_exports/exports.sh
+++ terminal/_exports/exports.sh
@@ -7,6 +7,9 @@ export FZF_DEFAULT_OPTS='
   --reverse
 '
 
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
 export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 
--- terminal/bash/.profile
+++ terminal/bash/.profile
@@ -3,6 +3,3 @@ export XENVIRONMENT="$HOME/.Xresources"
 
 #wmname LG3D
 ulimit -u 10000
-
-GITLAB_API_KEY="VGpgpBZ3spoCSty7rrPh"
-export GITLAB_API_KEY
